56f172a6f43b92d0a005ee7180dbef4d42f2f5ee
Sundwell <lyamtsev.s2017@gmail.com>
2024-11-04 22:28:02 +0100

    Updated UI for end screen

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,8 +1,19 @@
 extends CanvasLayer
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var panel_container: PanelContainer = $PanelContainer
+
 
 func _ready():
 	get_tree().paused = true
+	
+	panel_container.pivot_offset = panel_container.size / 2
+	panel_container.scale = Vector2.ZERO
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0.3) \
+			.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	%RestartButton.pressed.connect(on_restart_button_pressed)
 	%QuitButton.pressed.connect(on_quit_button_pressed)
 
@@ -13,7 +24,7 @@ func set_defeat():
 
 func on_restart_button_pressed():
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	get_tree().reload_current_scene()
 	
 	
 func on_quit_button_pressed():
